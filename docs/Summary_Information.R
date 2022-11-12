b47189d880dba0842f10fc267fee2ad05b761f7d
Rohan Simha <rohansimha@Rohans-MacBook-Air.local>
2022-11-11 16:01:48 -0800

    Submission for P2

---
+++ docs/Summary_Information.R
@@ -0,0 +1,38 @@
+# summary_info.R 
+# A source file that takes in a dataset and returns a list of info about it:
+#rm(list = ls())
+
+#load first data
+sleepdata<- read.csv(file = "../data/sleepdata.csv", sep = ";", quote = "")
+
+#load second data
+sleepdata_2<- read.csv(file = "../data/sleepdata_2.csv", sep = ";", quote = "")
+
+#load third data
+SaYodata<- read.csv(file = "../data/SaYoPillow.csv")
+
+
+#first data summary
+summary_info <- list()
+summary_info$num_observations <- nrow(sleepdata)
+summary_info$Sleep_quality_max_value <- sleepdata %>%
+  filter(Sleep.quality == max(Sleep.quality, na.rm = T)) %>%
+  select(Sleep.quality)
+
+#Second data summary
+
+summary_info_2 <- list()
+summary_info_2$num_observations <- nrow(sleepdata_2)
+summary_info_2$Steps_min_value <- sleepdata_2 %>%
+  filter(Steps == min(Steps, na.rm = T)) %>%
+  select(Steps)
+
+
+#Third data summary
+
+summary_info_3 <- list()
+summary_info_3$num_observations <- nrow(SaYodata)
+summary_info_3$stress_level_max_value <- SaYodata %>%
+  filter(sl == max(sl, na.rm = T)) %>%
+  select(sl)
+
