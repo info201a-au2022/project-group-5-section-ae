79b6ef8827521dfa942ac3217035ac4e38a01fb2
Junheepark0531 <rusu0531@uw.edu>
2022-11-11 16:32:33 -0800

    finished

--- source/chart_2.R
+++ source/chart_2.R
@@ -4,7 +4,7 @@ library("stringr")
 library("dplyr")
 library("tidyverse")
 install.packages("ggplot2") # once per machine
-library("ggplot2")          # in each relevant script
+library("ggplot2") # in each relevant script
 
 #Load the data
 sleepdata_2<- read.csv(file = "../data/sleepdata_2.csv", sep = ";", quote = "")
@@ -14,8 +14,9 @@ top_20 <- sleepdata_2 %>%
   top_n(20, wt = Steps) 
 
 # Render a horizontal bar chart
-ggplot(data=top_20, aes(x = Steps , y = Sleep.Quality))+
-  geom_bar(stat="identity",fill="purple") + 
+ggplot(data=top_20, aes(x = Steps , y = Sleep.Quality, group = 1))+
+  geom_line( color="#69b3a2", size=1.5, alpha=0.9, linetype = "dashed" ) +
+  geom_point() +
   xlab("Steps in Day") +
   ylab("Sleep Quality") +
   ggtitle("Relationship Between Sleep Quality and Steps in day")
