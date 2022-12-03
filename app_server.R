467b29a97fc67cf917e6c3228ddd4fd8a2427b9a
Ji Min Sung <jsung@Jis-MacBook-Pro-2.local>
2022-12-03 12:39:22 -0800

    updated

--- app_server.R
+++ app_server.R
@@ -6,6 +6,7 @@ library("tidyverse")
 library("ggplot2")
 library("plotly")
 library("rsconnect")
+library("stringr")
 
 # loading dataset
 sleepdata <- read.csv("./data/sleepdata.csv", sep = ";", quote = "")
@@ -29,4 +30,47 @@ server <- function(input, output){
                geom_line(color = "red") +
                labs(title = "How Stress Level Affects Your Sleep", x = "Snoring Rate", y = "Sleeping Time"))
   })
-}
+    
+    #render plotly
+    output$sleepScatter <- renderPlotly({
+      #wrangle the data for the bar graph using DPLYR
+      data <- sleepdata %>%
+        top_n(100, wt = Time.in.bed) %>%
+        filter(Wake.up == input$Wake.up) %>%
+        select(Time.in.bed, Sleep.quality)
+      
+      # Render a barplot with ggplotly
+      ggplotly(
+        ggplot(data = data, aes(x = Time.in.bed, y = Sleep.quality)) + 
+          geom_point() + 
+          ggtitle(input$Wake.up) +
+          xlab("Time in Bed") + 
+          ylab("Sleep Quality(in Percentage)")
+        
+      )
+    })
+    
+    #render plotly
+    output$sleepBar <- renderPlotly({
+      #wrangle the data for the bar graph using DPLYR
+      data_2 <- sleepdata_2 %>%
+        top_n(100, wt = Time.asleep..seconds.) %>%
+        filter(Alarm.mode == input$Alarm.mode) %>%
+        select(Time.asleep..seconds., Sleep.Quality)
+      
+      # Render a barplot with ggplotly
+      ggplotly(
+        ggplot(data = data_2, aes(x = Sleep.Quality, y = Time.asleep..seconds.)) + 
+          geom_bar(stat = "identity", fill = "blue") + 
+          ggtitle(input$Alarm.mode) +
+          xlab("Sleep Quality(in Percentage)") + 
+          ylab("Time alseep in seconds")
+        
+      )
+    })
+    
+  }
+
+
+
+
--- app_ui.R
+++ app_ui.R
@@ -4,6 +4,7 @@ library("tidyverse")
 library("ggplot2")
 library("plotly")
 library("rsconnect")
+library("shinyWidgets")
 
 # loading dataset
 
@@ -12,7 +13,17 @@ intro_view <- tabPanel(
   h1(strong("The Importance Of Sleep")),
   img("", src = "https://www.thehealthy.com/wp-content/uploads/2020/12/sleep-quotes08.jpg?fit=680,680",
       width = "400", height = "400", align = "center"),
-  p("Introduction goes here"),
+  p("Sleep is a necessity and a crucial part of everyday life. 
+    However, a lot of us have been experiencing a lack of sleep for various reasons, such as stress, 
+    or we just have busy and chaotic lives with school and work.
+    It made us curious about the biggest reasons of lack of sleep and how the amount of sleep affects us. 
+    In our project Sleep Well, we asked questions such as what is causing people to have a lack of sleep, 
+    what is the average amount of sleep that lets people feeling well rested, and how does sleep affect stress levels.
+    The stakeholders are people who experience insomnia, do not get enough sleep, have trouble sleeping, 
+    and people who experience high levels of stress because of poor sleep. 
+    This will benefit them because it can help them find out if there’s anything wrong with their sleep schedule, 
+    and their stress levels could be reduced after fixing their sleep. The data we analyzed is from Kaggle. 
+    Two of the datasets are data from an iOS app called Sleep Cycle. We used all the data to produce graphs and interactive charts shown below."),
   h2(strong("Data Analysis")),
   p("data analysis goes here")
 )
@@ -35,11 +46,80 @@ chart_view_1 <- tabPanel(
   )
 )
 
+page2_datas <- fluidPage(    
+  
+  # Give the page a title
+  titlePanel(strong("Relationships between Sleep quality and Time in Bed based on the emotion when wake up")),
+  
+  # Generate a row with a sidebar
+  sidebarLayout(      
+    
+    # Define the sidebar with a box of selections of countries, and the slider for the adjustments of year in bar plot
+    sidebarPanel(
+      selectInput("Wake.up", "Wake Up:", 
+                  choices = unique(sleepdata$Wake.up)),
+      helpText("Data from 'sleepdata.csv'")
+    ),
+    
+    # Create a spot for the bar plot and the chart
+    mainPanel(
+      plotlyOutput("sleepScatter")
+      
+    )
+    
+  )
+)
+
+####Tab Panel for Page 2####
+
+chart_view_2 <- tabPanel(
+  "Interactive Visualization", 
+  page2_datas
+)
+
+
+
+page3_datas <- fluidPage(    
+  
+  # Give the page a title
+  titlePanel(strong("Relationships between Sleep quality and Time asleep based on the Alarm mode")),
+  
+  # Generate a row with a sidebar
+  sidebarLayout(      
+    
+    # Define the sidebar with a box of selections of countries, and the slider for the adjustments of year in bar plot
+    sidebarPanel(
+      selectInput("Alarm.mode", "Alarm mode:", 
+                  choices = unique(sleepdata_2$Alarm.mode)),
+      helpText("Data from 'sleepdata_2.csv'")
+    ),
+    
+    # Create a spot for the bar plot and the chart
+    mainPanel(
+      plotlyOutput("sleepBar")
+      
+    )
+    
+  )
+)
+
+####Tab Panel for Page 2####
+
+chart_view_3 <- tabPanel(
+  "Interactive Visualization_2", 
+  page3_datas
+)
+
+
+
+
+
 # Define UI
 ui <- navbarPage(
   "Sleep Well",
   theme = shinythemes::shinytheme("cosmo"),
   intro_view,
-  chart_view_1
+  chart_view_1,
+  chart_view_2,
+  chart_view_3
 )
-
